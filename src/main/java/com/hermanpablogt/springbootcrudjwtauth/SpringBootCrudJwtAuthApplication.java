cb4dc33703f7275263aa95f39d66164e98b565f2
Herman Pablo Huitz Sac <inghuitzsac@outlook.com>
2023-06-08 22:34:32 +0200

    feat: create base project

---
+++ src/main/java/com/hermanpablogt/springbootcrudjwtauth/SpringBootCrudJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hermanpablogt.springbootcrudjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudJwtAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hermanpablogt/springbootcrudjwtauth/SpringBootCrudJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermanpablogt.springbootcrudjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
